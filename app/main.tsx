52794e61bd4c38506c73c5933bc2f6cb7dbead30
przemyslaw-zan <przemyslaw.zan@gmail.com>
2025-01-11 01:01:46 +0100

    Fixed build.

--- app/main.tsx
+++ app/main.tsx
@@ -3,12 +3,12 @@ import ReactDOM from 'react-dom/client';
 import { App } from './App.tsx';
 import { initLangs } from './utils/initLangs.ts';
 
-await initLangs();
-
-ReactDOM.createRoot( document.querySelector( '#app' )! ).render(
-	(
-		<StrictMode>
-			<App/>
-		</StrictMode>
-	)
-);
+initLangs().then( () => {
+	ReactDOM.createRoot( document.querySelector( '#app' )! ).render(
+		(
+			<StrictMode>
+				<App/>
+			</StrictMode>
+		)
+	);
+} );
